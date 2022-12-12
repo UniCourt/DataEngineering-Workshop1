1b428b59bc6515596887052488afdf498845bd03
nishan <bishankumar559@gmail.com>
2022-12-12 18:53:02 +0530

    first commit by Nishan

--- web_scraping_sample.py
+++ web_scraping_sample.py
@@ -1,4 +1,4 @@
-import requests
+"""import requests
 from bs4 import BeautifulSoup
 import re
 import psycopg2
@@ -28,4 +28,60 @@ for row in data.findAll("div"):
 file = open("qn_ans_ans", "w")
 
 for i in range(len(question_list)):
-    cursor.execute("insert into qn_ans values(%s,%s)", (question_list[i], answer_list[i]))
+    cursor.execute("insert into qn_ans values(%s,%s)", (question_list[i], answer_list[i]))"""
+    
+    
+#its for scraping 'https://blog.python.org/ ,this website. 
+    
+import requests
+from bs4 import BeautifulSoup
+
+
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="WebScrap",
+    user="nick",
+    password="Nishan@666"
+)
+conn.autocommit = True
+cur = conn.cursor()
+
+cur.execute("CREATE TABLE IF NOT EXISTS scrapeData(id SERIAL PRIMARY KEY, postDay VARCHAR, postHead VARCHAR, postBody VARCHAR, postAuthor VARCHAR);")
+
+
+getUrl = requests.get('https://blog.python.org/')
+recvHtml = getUrl.content
+
+bsObj = BeautifulSoup(recvHtml,'html.parser')
+
+pbar=""
+for post in bsObj.find_all("div",class_="date-outer"):
+	postVar=""
+	date = post.find("h2",class_="date-header")
+	dateVar = date.text
+
+
+	main_head = post.find("h3",class_="post-title entry-title")
+	headVar = main_head.text
+
+
+	for content_body in post.find("div", class_="post-body entry-content"): 
+
+		postVar = postVar+(content_body.text+"\n")
+
+	author_name = post.find("span",class_="fn")
+	authorVar = author_name.text
+
+
+	cur.execute("INSERT INTO scrapeData (postDay,postHead,postBody,postAuthor) VALUES(%s,%s,%s,%s)",[dateVar,headVar,postVar,authorVar])
+
+	pbar=pbar+"==="
+	print(pbar)
+
+
+print("Data Scraped and stored in the Database sucessfully")
+cur.close()
+conn.close()
+
